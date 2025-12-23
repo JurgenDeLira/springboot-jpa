be90c6e437fafe95aa03c25bd679b09edff502f9
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-12-22 17:14:25 -0700

    Entity

---
+++ src/main/java/com/jorge/jpa/springboot_jpa/entities/Person.java
@@ -0,0 +1,35 @@
+package com.jorge.jpa.springboot_jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "persons")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastname;
+
+    @Column(name = "programming_languange")
+    private String programmingLanguage;
+
+    @Override
+    public String toString() {
+        return
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", programmingLanguage='" + programmingLanguage;
+    }
+}
